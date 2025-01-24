f7d20d15df4c8c9d76ca85ec38e53ec6c1467726
yash.d.habib1@gmail.com <yash.habib@lief.care>
2025-01-24 12:11:32 +0530

    vercel config

--- backend/server.js
+++ backend/server.js
@@ -33,16 +33,16 @@ const __dirname = path.resolve();
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
   app.get("/", (req, res) => {
     res.send("Api is running... Yash");
   });
-}
+// }
 
 app.use(notfound);
 
